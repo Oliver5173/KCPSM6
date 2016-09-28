5fac4e00561a58665793976c5e2b3c94415fb4d2
goldenpartner <liuyeefan@outlook.com>
2016-09-28 16:53:00 -0700

    Add files via upload

--- func_helper.jl
+++ func_helper.jl
@@ -1,4 +1,4 @@
-regs = ['']
+regs = ["s0","s1","s2","s3","s4","s5","s6","s7","s8","s9","sA","sB","sC","sD","sE","sF"]
 
 function func_return(operation::String, operand1::String, operand2::String)
  if operation == 'LOAD'
@@ -8,6 +8,9 @@ end
 
 
 
-function load(operand1, operand2)
-  operand2
+function load(operand1, operand2,regs)
+  if operand2 in regs
+    return bank[operand2]
+  else
+  end
 end
--- functions.jl
+++ functions.jl
@@ -6,36 +6,36 @@ operand2 =
 
 for operation in command["operation"]
   if operand1 == "s0"
-    s0 = func_return(operation, operand1, operand2)
+    Bank_main.s0 = func_return(operation, operand1, operand2)
   else if operand1 == "s1"
-    s1 = func_return(operation, operand1, operand2)
+    Bank_main.s1 = func_return(operation, operand1, operand2)
   else if operand1 == "s2"
-    s2 = func_return(operation, operand1, operand2)
+    Bank_main.s2 = func_return(operation, operand1, operand2)
   else if operand1 == "s3"
-    s3 = func_return(operation, operand1, operand2)
+    Bank_main.s3 = func_return(operation, operand1, operand2)
   else if operand1 == "s4"
-    s4 = func_return(operation, operand1, operand2)
+    Bank_main.s4 = func_return(operation, operand1, operand2)
   else if operand1 == "s5"
-    s5 = func_return(operation, operand1, operand2)
+    Bank_main.s5 = func_return(operation, operand1, operand2)
   else if operand1 == "s6"
-    s6 = func_return(operation, operand1, operand2)
+    Bank_main.s6 = func_return(operation, operand1, operand2)
   else if operand1 == "s7"
-    s7 = func_return(operation, operand1, operand2)
+    Bank_main.s7 = func_return(operation, operand1, operand2)
   else if operand1 == "s8"
-    s8 = func_return(operation, operand1, operand2)
+    Bank_main.s8 = func_return(operation, operand1, operand2)
   else if operand1 == "s9"
-    s9 = func_return(operation, operand1, operand2)
+    Bank_main.s9 = func_return(operation, operand1, operand2)
   else if operand1 == "sA"
-    sA = func_return(operation, operand1, operand2)
+    Bank_main.sA = func_return(operation, operand1, operand2)
   else if operand1 == "sB"
-    sB = func_return(operation, operand1, operand2)
+    Bank_main.sB = func_return(operation, operand1, operand2)
   else if operand1 == "sC"
-    sC = func_return(operation, operand1, operand2)
+    Bank_main.sC = func_return(operation, operand1, operand2)
   else if operand1 == "sD"
-    sD = func_return(operation, operand1, operand2)
+    Bank_main.sD = func_return(operation, operand1, operand2)
   else if operand1 == "sE"
-    sE = func_return(operation, operand1, operand2)
+    Bank_main.sE = func_return(operation, operand1, operand2)
   else if operand1 == "sF"
-    sF = func_return(operation, operand1, operand2)
+    Bank_main.sF = func_return(operation, operand1, operand2)
   end
 end
