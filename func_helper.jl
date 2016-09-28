70c4d939ba5c21f8d56799912ae2a974d3736a31
goldenpartner <liuyeefan@outlook.com>
2016-09-28 16:23:45 -0700

    Add files via upload

---
+++ func_helper.jl
@@ -0,0 +1,13 @@
+regs = ['']
+
+function func_return(operation::String, operand1::String, operand2::String)
+ if operation == 'LOAD'
+   return
+ end
+end
+
+
+
+function load(operand1, operand2)
+  operand2
+end
---
+++ functions.jl
@@ -0,0 +1,41 @@
+include("func_helper.jl")
+
+
+operand1 = command["operand1"]
+operand2 =
+
+for operation in command["operation"]
+  if operand1 == "s0"
+    s0 = func_return(operation, operand1, operand2)
+  else if operand1 == "s1"
+    s1 = func_return(operation, operand1, operand2)
+  else if operand1 == "s2"
+    s2 = func_return(operation, operand1, operand2)
+  else if operand1 == "s3"
+    s3 = func_return(operation, operand1, operand2)
+  else if operand1 == "s4"
+    s4 = func_return(operation, operand1, operand2)
+  else if operand1 == "s5"
+    s5 = func_return(operation, operand1, operand2)
+  else if operand1 == "s6"
+    s6 = func_return(operation, operand1, operand2)
+  else if operand1 == "s7"
+    s7 = func_return(operation, operand1, operand2)
+  else if operand1 == "s8"
+    s8 = func_return(operation, operand1, operand2)
+  else if operand1 == "s9"
+    s9 = func_return(operation, operand1, operand2)
+  else if operand1 == "sA"
+    sA = func_return(operation, operand1, operand2)
+  else if operand1 == "sB"
+    sB = func_return(operation, operand1, operand2)
+  else if operand1 == "sC"
+    sC = func_return(operation, operand1, operand2)
+  else if operand1 == "sD"
+    sD = func_return(operation, operand1, operand2)
+  else if operand1 == "sE"
+    sE = func_return(operation, operand1, operand2)
+  else if operand1 == "sF"
+    sF = func_return(operation, operand1, operand2)
+  end
+end
