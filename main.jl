b192b928f6d77b45a94064edf90c8c5a5262a44e
Oliver5173 <gormory@Outlook.com>
2016-09-28 20:06:07 -0700

    Update main.jl

--- main.jl
+++ main.jl
@@ -1,18 +1,38 @@
-include("reg.jl")
 include("program_counter.jl")
-
-import reg,pc
-
+import pc
 #define banks and cpu flags
-BankA = reg.bank(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-BankB = reg.bank(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-Bank_main = reg.bank(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
-#flags(Z::Int,C::Int,IE::Int,REGBANK::Char)
-flag = pc.flags(0,0,0,'A')
+println("")
+BankA = Array{UInt8}(16)
+BankB = Array{UInt8}(16)
 
-#opreation here
-if flag.REGBANK == 'A'
-	Bank_main = BankA
-else
-	Bank_main = BankB
+type flags
+	Z :: Int
+	C :: Int
+	IE :: Int
+	REGBANK :: Bool
 end
+
+flag = flags(0,0,0,true)
+
+println("-----------KCPSM6 simulated-----------")
+
+const FILE_NAME = "test.asm"
+
+#cheak file existance 
+if !isfile(FILE_NAME)
+	println(FILE_NAME," DOES NOT EXIST\nProgram exit in 5s")
+	sleep(4)
+	exit()
+end
+
+println("Reading Assembly Code ... ...")
+
+#loading file to code_arr
+f = open(FILE_NAME)
+length_of_line = length(readlines(f))
+f = open(FILE_NAME)
+code_arr = Array{String}(0)
+for i = 1:length_of_line
+  push!(code_arr,readline(f))
+end
+close(f)
