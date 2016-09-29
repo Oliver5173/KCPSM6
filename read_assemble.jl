8bd027468eb6f97545f10bf5d76c12b54208c377
chhs1129 <chhs1129@gmail.com>
2016-09-28 20:48:58 -0700

    Update read_assemble.jl

--- read_assemble.jl
+++ read_assemble.jl
@@ -10,87 +10,87 @@ type instruction
 	operand2
 end
 
-function load_code(code_of_line::Int)
+function load_code(length_of_line::Int)
   label_arr = Array{label}(0)
   instruction_arr = Array{instruction}(0)
   for i = 1 : length_of_line
 
   	j=1
-  	input=code_arr[i]
+  	eachline=code_arr[i]
 
-  	last = findfirst(input, '\\')
-  	k = findfirst(input, ':')
+  	last = findfirst(eachline, '\\')
+  	k = findfirst(eachline, ':')
   	if k != 0
 
-  		push!(label_arr,label(input[j:k-1],i))
+  		push!(label_arr,label(eachline[j:k-1],i))
   		j=k+2
 
-  		k=findfirst(input[j:last] , ' ')
+  		k=findfirst(eachline[j:last] , ' ')
   		if k != 0
-  			temp1 = input[j:k-1]
+  			temp1 = eachline[j:k-1]
   			j=k+1
 
-  			k=findfirst(input[j:last], ',')
+  			k=findfirst(eachline[j:last], ',')
   			if k != 0
-  				temp2 = input[j:k-1]
+  				temp2 = eachline[j:k-1]
   				j=k+1
 
-  				temp3=input[j:last]
+  				temp3=eachline[j:last]
   				h=temp3[end-1:end]
   				if(h=="'d")
       				p=c[1:end-2]
       				p=parse(p)
-      				output=hex(p)
+      				output_string=hex(p)
 
     					elseif (h=="'b")
       				p=c[1:end-2]
       				p=string("0b",p)
-      				output=parse(p)
-      				output=string(output)
-      				output=parse(output)
-      				output=hex(output)
+      				output_string=parse(p)
+      				output_string=string(output_string)
+      				output_string=parse(output_string)
+      				output_string=hex(output_string)
     				end
-  				push!(instruction_arr,instruction(temp1, temp2,output))
+  				push!(instruction_arr,instruction(temp1, temp2,output_string))
   			else
   				push!(instruction_arr,instruction(temp1, temp2, ""))
   			end
   		else
-  			push!(instruction_arr,instruction(input[j:last-1], "", ""))
+  			push!(instruction_arr,instruction(eachline[j:last-1], "", ""))
   		end
 
   	else
 
-  		k=findfirst(input[j:last] , ' ')
+  		k=findfirst(eachline[j:last] , ' ')
   		if k != 0
-  			temp1 = input[j:k-1]
+  			temp1 = eachline[j:k-1]
   			j=k+1
 
-  			k=findfirst(input[j:last], ',')
+  			k=findfirst(eachline[j:last], ',')
   			if k != 0
-  				temp2 = input[j:k-1]
+  				temp2 = eachline[j:k-1]
   				j=k+1
 
-  				temp3=input[j:last]
+  				temp3=eachline[j:last]
   				h=temp3[end-1:end]
   				if(h=="'d")
       				p=c[1:end-2]
       				p=parse(p)
-      				output=hex(p)
+      				output_string=hex(p)
 
     					elseif (h=="'b")
       				p=c[1:end-2]
       				p=string("0b",p)
-      				output=parse(p)
-      				output=string(output)
-      				output=parse(output)
-      				output=hex(output)
+      				output_string=parse(p)
+      				output_string=string(output_string)
+      				output_string=parse(output_string)
+      				output_string=hex(output_string)
     				end
-  				push!(instruction_arr,instruction(temp1, temp2,output))
+  				push!(instruction_arr,instruction(temp1, temp2,output_string))
   			else
   				push!(instruction_arr,instruction(temp1, temp2, ""))
   			end
   		else
-  			push!(instruction_arr,instruction(input[j:last-1], "", ""))
+  			push!(instruction_arr,instruction(eachline[j:last-1], "", ""))
   		end
   	end
   end
