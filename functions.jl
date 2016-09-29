d6a9beb59d85b140f451838bc9ea6c459bc28300
goldenpartner <liuyeefan@outlook.com>
2016-09-28 20:52:35 -0700

    Add files via upload

--- functions.jl
+++ functions.jl
@@ -1,8 +1,8 @@
 include("func_helper.jl")
 while i = 1:length(operation)
-  operand1 = opd1[i]
-  operand2 = opd2[i]
-  operation = op[i]
+  operand1 = a[i].operand1
+  operand2 = a[i].operand2
+  operation = a[i].op_code
   if operation =="REGBANK"
     if using_A && operand1 == "A"
       continue
@@ -10,7 +10,7 @@ while i = 1:length(operation)
       continue
     else
       using_A = !using_A
-      
+
   elseif operation == "LOAD"
     load(operand1, operand2, BankA, BankB, using_A)
 
