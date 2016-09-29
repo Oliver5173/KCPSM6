43c9cf7c0f777506148e6156b8da9e36b4207d86
chhs1129 <chhs1129@gmail.com>
2016-09-28 20:46:41 -0700

    Update read_assemble.jl

--- read_assemble.jl
+++ read_assemble.jl
@@ -1,3 +1,4 @@
+module trans
 type label
 	label_name :: String
 	index :: Int
@@ -9,87 +10,91 @@ type instruction
 	operand2
 end
 
-label_arr = Array{label}(0)
-instruction_arr = Array{instruction}(0)
+function load_code(code_of_line::Int)
+  label_arr = Array{label}(0)
+  instruction_arr = Array{instruction}(0)
+  for i = 1 : length_of_line
 
-for i = 1 : length(code_arr)
-	
-	j=1
-	input=code_arr[i]
+  	j=1
+  	input=code_arr[i]
 
-	last = findfirst(input, '\\')
-	k = findfirst(input, ':')
-	if k != 0
+  	last = findfirst(input, '\\')
+  	k = findfirst(input, ':')
+  	if k != 0
 
-		push!(label_arr,label(input[j:k-1],i))
-		j=k+2
+  		push!(label_arr,label(input[j:k-1],i))
+  		j=k+2
 
-		k=findfirst(input[j:last] , ' ')
-		if k != 0
-			temp1 = input[j:k-1]
-			j=k+1
+  		k=findfirst(input[j:last] , ' ')
+  		if k != 0
+  			temp1 = input[j:k-1]
+  			j=k+1
 
-			k=findfirst(input[j:last], ',')
-			if k != 0
-				temp2 = input[j:k-1]
-				j=k+1
+  			k=findfirst(input[j:last], ',')
+  			if k != 0
+  				temp2 = input[j:k-1]
+  				j=k+1
 
-				temp3=input[j:last]
-				h=temp3[end-1:end]
-				if(h=="'d")
-    				p=c[1:end-2]
-    				p=parse(p)
-    				output=hex(p)
-    				
-  					elseif (h=="'b")
-    				p=c[1:end-2]
-    				p=string("0b",p)
-    				output=parse(p)
-    				output=string(output)
-    				output=parse(output)
-    				output=hex(output)
-  				end
-				push!(instruction_arr,instruction(temp1, temp2,output))
-			else
-				push!(instruction_arr,instruction(temp1, temp2, ""))
-			end
-		else
-			push!(instruction_arr,instruction(input[j:last-1], "", ""))
-		end
+  				temp3=input[j:last]
+  				h=temp3[end-1:end]
+  				if(h=="'d")
+      				p=c[1:end-2]
+      				p=parse(p)
+      				output=hex(p)
 
-	else
+    					elseif (h=="'b")
+      				p=c[1:end-2]
+      				p=string("0b",p)
+      				output=parse(p)
+      				output=string(output)
+      				output=parse(output)
+      				output=hex(output)
+    				end
+  				push!(instruction_arr,instruction(temp1, temp2,output))
+  			else
+  				push!(instruction_arr,instruction(temp1, temp2, ""))
+  			end
+  		else
+  			push!(instruction_arr,instruction(input[j:last-1], "", ""))
+  		end
 
-		k=findfirst(input[j:last] , ' ')
-		if k != 0
-			temp1 = input[j:k-1]
-			j=k+1
+  	else
 
-			k=findfirst(input[j:last], ',')
-			if k != 0
-				temp2 = input[j:k-1]
-				j=k+1
+  		k=findfirst(input[j:last] , ' ')
+  		if k != 0
+  			temp1 = input[j:k-1]
+  			j=k+1
 
-				temp3=input[j:last]
-				h=temp3[end-1:end]
-				if(h=="'d")
-    				p=c[1:end-2]
-    				p=parse(p)
-    				output=hex(p)
+  			k=findfirst(input[j:last], ',')
+  			if k != 0
+  				temp2 = input[j:k-1]
+  				j=k+1
 
-  					elseif (h=="'b")
-    				p=c[1:end-2]
-    				p=string("0b",p)
-    				output=parse(p)
-    				output=string(output)
-    				output=parse(output)
-    				output=hex(output)
-  				end
-				push!(instruction_arr,instruction(temp1, temp2,output))
-			else
-				push!(instruction_arr,instruction(temp1, temp2, ""))
-			end
-		else
-			push!(instruction_arr,instruction(input[j:last-1], "", ""))
-		end
-	end
+  				temp3=input[j:last]
+  				h=temp3[end-1:end]
+  				if(h=="'d")
+      				p=c[1:end-2]
+      				p=parse(p)
+      				output=hex(p)
+
+    					elseif (h=="'b")
+      				p=c[1:end-2]
+      				p=string("0b",p)
+      				output=parse(p)
+      				output=string(output)
+      				output=parse(output)
+      				output=hex(output)
+    				end
+  				push!(instruction_arr,instruction(temp1, temp2,output))
+  			else
+  				push!(instruction_arr,instruction(temp1, temp2, ""))
+  			end
+  		else
+  			push!(instruction_arr,instruction(input[j:last-1], "", ""))
+  		end
+  	end
+  end
+  return label_arr,instruction_arr
+end
+
 end
