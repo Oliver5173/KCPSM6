c4032b27afce176b2a960ff6a9f6c92951d97206
Oliver5173 <gormory@Outlook.com>
2016-09-28 18:21:14 -0700

    Update reg.jl
    change type bank to array

--- reg.jl
+++ reg.jl
@@ -1,22 +1,5 @@
 module reg
-  type bank
-    s0 :: UInt8
-    s1 :: UInt8
-    s2 :: UInt8
-    s3 :: UInt8
-    s4 :: UInt8
-    s5 :: UInt8
-    s6 :: UInt8
-    s7 :: UInt8
-    s8 :: UInt8
-    s9 :: UInt8
-    sA :: UInt8
-    sB :: UInt8
-    sC :: UInt8
-    sD :: UInt8
-    sE :: UInt8
-    sF :: UInt8
-  end
+  Bank = Array{UInt8}(16)
   type flags
     Z :: Int
     C :: Int
